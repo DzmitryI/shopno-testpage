090173eb526ad37845c070dc9aaf040c353363bb
Dzmitry Ivanou <3prn@mail.ru>
2020-01-19 21:35:54 +0300

    init gh-pages

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './screens/index.scss';
-import './screens/slider';
-import './screens/slider1';
-import './screens/sliderReview';
-import './screens/modalPage';
-import './screens/services';
